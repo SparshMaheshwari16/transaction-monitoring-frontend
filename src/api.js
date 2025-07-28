cdd60b1afc1edd1c26f3220ca09e875aa225fbb5
Sparsh Maheshwari <sparsh.m@algoritt.com>
2025-07-28 19:12:48 +0530

    bug: Added .env

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_KEY = "ab5a5840-7eca-4d14-8e74-7eb9d346feb1"; // Replace with your actual API key
+const API_KEY = process.env.REACT_APP_API_KEY; // API key
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
         "x-api-key": API_KEY,
     },
