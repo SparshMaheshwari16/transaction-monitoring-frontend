07bfd7a95551fcd8af2ee18af469048a804ad98a
Sparsh Maheshwari <sparsh.m@algoritt.com>
2025-08-02 14:53:52 +0530

    feat(env): update env

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY; // API key
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
         "x-api-key": API_KEY,
     },
